97765bf1fc6f31190f71ea36e6adcddbf7b791c3
Florian <FloVogl@protonmail.com>
2020-04-02 12:24:53 +0200

    correct typo

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@
 [ -d $HOME/.local/share ] || mkdir -p $HOME/.local/share
 [ -d $HOME/.local/bin ] || mkdir -p $HOME/.local/bin
 
-cp -r src/dwnstatus-src $HOME/.local/share/dwmstatus
+cp -r src/dwmstatus-src $HOME/.local/share/dwmstatus
 cp src/dwmstatus $HOME/.local/bin/dwmstatus
